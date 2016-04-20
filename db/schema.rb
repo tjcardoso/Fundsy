ce5afefb08a1731fdee5c170ec6b1d385316552b
Tammam Kbeili <tammam.kbeili@gmail.com>
2016-04-20 11:13:28 -0700

    test drive user model and users controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412183932) do
+ActiveRecord::Schema.define(version: 20160420161617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20160412183932) do
     t.datetime "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it "requires a first name" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({first_name: nil})
+      expect(u).to be_invalid
+    end
+
+    it "requies a last name" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({last_name: nil})
+      expect(u).to be_invalid
+    end
+
+    it "requiers an email" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({email: nil})
+      expect(u).to be_invalid
+    end
+
+    it "requires a unique email" do
+      u  = FactoryGirl.create(:user)
+      u2 = User.new FactoryGirl.attributes_for(:user).merge({email: u.email})
+      expect(u2).to be_invalid
+    end
+  end
+
+  describe ".full_name" do
+    it "returns concatenated first and last names" do
+      u = User.new FactoryGirl.attributes_for(:user).
+                        merge({first_name: "John", last_name: "Smith"})
+      expect(u.full_name).to eq("John Smith")
+    end
+  end
+
+  describe "hashing the password" do
+    it "generates a password digest" do
+      u = User.new FactoryGirl.attributes_for(:user)
+      u.save
+      expect(u.password_digest).to be
+    end
+  end
+end
